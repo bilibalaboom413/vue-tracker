be5d4f6c64be0b04249f658d635d4bb8983cba6a
Hao Chen <hche8093@uni.sydney.edu.au>
2022-08-09 11:22:09 +1000

    Add Task

---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button text="Add Task" color="green" />
+  </header>
+</template>
+
+<script>
+import Button from './Button'
+
+  export default {
+    // eslint-disable-next-line vue/multi-word-component-names
+    name: 'Header',
+    props: {
+      title: {
+        type: String,
+      }
+    },
+    components: {
+      Button,
+    }
+  }
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-content: center;
+  margin-bottom: 20px;
+}
+</style>
